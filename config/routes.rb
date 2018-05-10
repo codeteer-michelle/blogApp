0bfe303df8e42d83e449cd0658c5d36fc248068a
codeteer-michelle <codeteer.michelle@gmail.com>
2018-05-10 15:15:33 -0400

    Added pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   end
   
   root "posts#index"
+
+# Below, we don't want to have to go to /pages/about, we just want to go to /about
+  get '/about', to: 'pages#about'
+
 end
